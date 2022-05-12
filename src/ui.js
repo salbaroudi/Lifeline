9c7fb8cf4602cc0da362b9cc85e53d9ad4514eb1
User <sean.al.baroudi@gmail.com>
2022-05-12 15:31:35 +0100

    Basic Frontend with CSS3+JQuery done.

--- src/ui.js
+++ src/ui.js
@@ -1,14 +1,58 @@
 
+
+let selectedWords = [];
+let sWLimit = false
+
+//Read out the list, and put into boxes innerHTML.
+function confirmfinalwords() {
+  let finalContent = "<span class='blinking'>";
+  for (let word of selectedWords) {
+    finalContent += ( word + " , ");
+  }
+  finalContent+="</span>";
+  $( "#selectedwords" ).html(finalContent);
+  $("#consolebox").attr("class","console");
+}
+
+//Once limit hits, we can no longer add words unless we press reset button.
+function modifyselwords(word) {
+  selectedWords.push(word);
+  if (selectedWords.length == 4) {
+    sWLimit = true;
+    confirmfinalwords();
+  }
+}
+
+//Does the css change, and calls modifyselwords() if not chosen yet.
+function addword(event) {
+  let classes = event.target.getAttribute("class").split(" ");
+  if (!classes.includes("clicked")) {
+    event.target.setAttribute("class", "grid-item-clicked " + classes[1]);
+    modifyselwords(event.target.innerHTML);
+  }
+}
+
+//State dependent block on addword() function.
+function wordpicked(event) {
+ (!sWLimit)?addword(event):null;
+}
+
+function resetwords(event) {
+  selectedWords = [];
+  sWLimit = false;
+  $(".grid-item-clicked").each( function(){
+    let classes = $(this).attr("class").split(" ");
+    console.log(classes);
+    $(this).attr("class","grid-item " + classes[1]);
+  });
+  $( "#selectedwords" ).html("");
+}
+
+$(".grid-item").on( "click", wordpicked);
+$("#reset-button").on( "click", resetwords);
+$("#confirm-words").on( "click", resetwords);
+
+
 // https://www.digitalocean.com/community/tutorials/how-to-modify-attributes-classes-and-styles-in-the-dom
-//https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
-
-function clickOn(event) {
-  console.log("clicked");
-  event.target.textContent = "It WORKED!!";
- }
-
-
-document.querySelectorAll('.grid-item').forEach(item => {
-  item.addEventListener('click', clickOn);
-  console.log("added");
-});
+// https://api.jquery.com/on/
+// Blinking Text CSS3: https://html-online.com/articles/blinking-text-css-animation/
